457c781c223a00b8424a7474ed3b8ccdfca2993c
Pravish <pravisharodrigo7@gmail.com>
2023-05-28 02:21:32 +0530

    project CNN

---
+++ app/ai.py
@@ -0,0 +1,58 @@
+import keras
+from keras.models import load_model
+import pickle
+import tensorflow as tf
+
+def clean_data(input_val):
+
+    input_val=input_val.replace('\n', '')
+    input_val=input_val.replace('%20', ' ')
+    input_val=input_val.replace('=', ' = ')
+    input_val=input_val.replace('((', ' (( ')
+    input_val=input_val.replace('))', ' )) ')
+    input_val=input_val.replace('(', ' ( ')
+    input_val=input_val.replace(')', ' ) ')
+    input_val=input_val.replace('1 ', 'numeric')
+    input_val=input_val.replace(' 1', 'numeric')
+    input_val=input_val.replace("'1 ", "'numeric ")
+    input_val=input_val.replace(" 1'", " numeric'")
+    input_val=input_val.replace('1,', 'numeric,')
+    input_val=input_val.replace(" 2 ", " numeric ")
+    input_val=input_val.replace(' 3 ', ' numeric ')
+    input_val=input_val.replace(' 3--', ' numeric--')
+    input_val=input_val.replace(" 4 ", ' numeric ')
+    input_val=input_val.replace(" 5 ", ' numeric ')
+    input_val=input_val.replace(' 6 ', ' numeric ')
+    input_val=input_val.replace(" 7 ", ' numeric ')
+    input_val=input_val.replace(" 8 ", ' numeric ')
+    input_val=input_val.replace('1234', ' numeric ')
+    input_val=input_val.replace("22", ' numeric ')
+    input_val=input_val.replace(" 8 ", ' numeric ')
+    input_val=input_val.replace(" 200 ", ' numeric ')
+    input_val=input_val.replace("23 ", ' numeric ')
+    input_val=input_val.replace('"1', '"numeric')
+    input_val=input_val.replace('1"', '"numeric')
+    input_val=input_val.replace("7659", 'numeric')
+    input_val=input_val.replace(" 37 ", ' numeric ')
+    input_val=input_val.replace(" 45 ", ' numeric ')
+
+    return input_val
+
+
+
+def predict(text):
+    
+    model = tf.keras.models.load_model('model.h5')
+    vectorizer = pickle.load(open("vectorizer_cnn", 'rb'))
+
+    input_val=clean_data(text)
+    input_val=[input_val]
+    
+    input_val=vectorizer.transform(input_val).toarray()
+    input_val.shape=(1,64,64,1)
+    result=model.predict(input_val)
+    
+    if result>0.5:
+        return 1 # Detect as as a SQL injection
+    elif result<=0.5:
+        return 0 # Detect as a normal query
